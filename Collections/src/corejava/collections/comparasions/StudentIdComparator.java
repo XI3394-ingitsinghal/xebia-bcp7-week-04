105bfbf79fa76909a9c848bf89fc79222f8352bf
XI3394-ingitsinghal <ingit.singhal@xebia.com>
2022-03-21 15:42:35 +0530

    Update comparator method

--- Collections/src/corejava/collections/comparasions/StudentIdComparator.java
+++ Collections/src/corejava/collections/comparasions/StudentIdComparator.java
@@ -1,5 +1,16 @@
 package corejava.collections.comparasions;
 
-public class StudentIdComparator {
+import java.util.Comparator;
+
+public class StudentIdComparator implements Comparator<Student> {
+		   public int compare(Student student1, Student student2) {
+		      if (student1.getId() < student2.getId()) {
+		         return -1;        
+		      }else if (o1.getId() > o2.getId()) {          
+		         return 1;
+		      } else {
+		         return 0;        
+		      }
+		   }
 
 }
