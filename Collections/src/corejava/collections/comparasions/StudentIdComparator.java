7ea66ba91ab46983d7121fa1c76c74e0d1843d87
XI3394-ingitsinghal <ingit.singhal@xebia.com>
2022-03-21 15:46:03 +0530

    Updated and formatted the code

--- Collections/src/corejava/collections/comparasions/StudentIdComparator.java
+++ Collections/src/corejava/collections/comparasions/StudentIdComparator.java
@@ -3,14 +3,14 @@ package corejava.collections.comparasions;
 import java.util.Comparator;
 
 public class StudentIdComparator implements Comparator<Student> {
-		   public int compare(Student student1, Student student2) {
-		      if (student1.getId() < student2.getId()) {
-		         return -1;        
-		      }else if (o1.getId() > o2.getId()) {          
-		         return 1;
-		      } else {
-		         return 0;        
-		      }
-		   }
+	public int compare(Student student1, Student student2) {
+		if (student1.getId() < student2.getId()) {
+			return -1;
+		} else if (student1.getId() > student2.getId()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 
 }
