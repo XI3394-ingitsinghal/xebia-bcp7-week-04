764ebd8f98809395d1c964fab65b75c8c7d7f436
XI3394-ingitsinghal <ingit.singhal@xebia.com>
2022-03-21 23:20:28 +0530

    Minor changes

--- Annotations/src/corejava/annotations/depreciated/Test.java
+++ Annotations/src/corejava/annotations/depreciated/Test.java
@@ -2,11 +2,12 @@ package corejava.annotations.depreciated;
 
 public class Test {
 	@Deprecated
-	  public void display() {
-	    System.out.println("display() method of Test class");
-	  }
-	  public static void main(String args[]) {
-	    Test obj = new Test();
-	    obj.display();
-	  }
+	public void display() {
+		System.out.println("display() method of Test class");
+	}
+
+	public static void main(String args[]) {
+		Test obj = new Test();
+		obj.display();
+	}
 }
